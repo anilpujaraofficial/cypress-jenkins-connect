91d075a1436d79f76892446a08c42781fe76d54e
Anil Pujara <anilpujara125@gmail.com>
2022-10-23 11:04:33 +0545

    update jenkins file

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -27,3 +27,8 @@ require("cypress-dark");
 
 import "cypress-if";
 require("cypress-mailosaur");
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false;
+});
